6564ec41dd9660a8a12ad575b11987dcb2dce210
Davis <davisperchik@gmail.com>
2019-03-16 12:26:24 -0400

    created add and get methods

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -3,5 +3,62 @@ package com.zipcodewilmington.singlylinkedlist;
 /**
  * Created by leon on 1/10/18.
  */
-public class SinglyLinkedList {
+public class SinglyLinkedList<T> {
+
+    Node head;
+    Node tempNode;
+
+    public SinglyLinkedList() {
+        head = new Node();
+    }
+
+    public void add(T element) {
+        tempNode = head;
+        while(tempNode.getNext() != null){
+          tempNode = tempNode.getNext();
+        }
+        tempNode.setNext(new Node(element,null));
+    }
+
+    public Object get(int index) {
+        tempNode = head;
+        int counter = 0;
+
+        while(counter != index){
+            tempNode = tempNode.getNext();
+            counter++;
+        }
+        return tempNode.getElement();
+    }
+
+
+    private class Node<T>{
+        T element;
+        Node next;
+
+        private Node(T element, Node next){
+            this.element = element;
+            this.next= next;
+        }
+
+        private Node(){
+            this(null, null);
+        }
+
+        public T getElement() {
+            return element;
+        }
+
+        public void setElement(T element) {
+            this.element = element;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+    }
 }
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,7 +1,24 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedListTest {
+
+    @Test
+    public void addTest(){
+        SinglyLinkedList singlyLinkedList = new SinglyLinkedList();
+
+        String hey = "Hey";
+        singlyLinkedList.add(hey);
+
+        String actual = (String) singlyLinkedList.get(1);
+
+        String expected = "Hey";
+
+        Assert.assertEquals(expected, actual);
+    }
 }
